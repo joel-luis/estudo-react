57130b24c5b41b0b158f531a50b7979457c2c858
Joel Luis <joelluis73@gmail.com>
2020-10-24 15:00:04 -0300

    Projetos de Estudo React

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header/index';
+import './styles.css';
+import Routes from './routes';
+
+const App = () => (
+
+  <div className="App">
+
+    <Header />
+    <Routes />
+    
+
+  </div>
+);
+ 
+
+
+export default App;
---
+++ src/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './Header.css';
+
+const Header = () => (
+
+    <div className='header'>
+        <header>JSHUNT</header>
+    </div>
+    
+);
+
+export default Header;
